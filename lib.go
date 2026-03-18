7b1905ad8a1e4bdce6097c859e619b9df8984681
Timur Symzhitov <t.symzhitov@redspher.com>
2026-03-18 10:28:46 +0900

    Module docs

--- lib.go
+++ lib.go
@@ -1,5 +1,9 @@
+// Package for calculations
 package broccoli
 
+// Add takes 2 integers and returns a [sum] of them
+//
+// [sum]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
